b590241d474ff2191e7be30e719732ab810b5ac6
yadavkajal <kajalyadav459@gmail.com>
2025-04-20 01:56:53 +0530

    fibonacci logic

--- FibonacciSeries.py
+++ FibonacciSeries.py
@@ -1,11 +1,45 @@
-# Fibonacci Series up to N Terms
+# 1.	Fibonacci Series up to N Terms
 # Write a program that prints the Fibonacci sequence up to the N-th term using a loop.
-num=int(input("Enter number till you want to print fibonacci"))
-def fibonacci(num):
-    for i in range(1,num):
-        sum=(i-1)+(i-2)
-        print(sum)
-fibonacci(num)
+
+# num=int(input("Enter a number of fibonacci series count"))
+# list=[0,1,1]
+
+# def fact(num):
+#     before=1
+#     current=1
+#     for i in range(0,num-(len(list))):
+        
+#         sum=before+current
+#         before=current
+#         current=sum
+#         list.append(sum)
+
+# fact(num)
+# print(list)
+
+#or
+num=int(input("Enter a number of fibonacci series count"))
+list=[0,1,1]
+
+def fact(num):
+    before=1
+    current=1
+    while num-(len(list))>0:
+        
+        sum=before+current
+        before=current
+        current=sum
+        list.append(sum)
+
+fact(num)
+print(list)
 
 
-    
+""" 
+before = 1
+current =1 
+sum=before+current=2
+before=current
+current=sum
+
+"""
