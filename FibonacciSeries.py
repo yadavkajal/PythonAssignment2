f092c927ce4231ced3fbe72d84ee91c00484e4a2
yadavkajal <kajalyadav459@gmail.com>
2025-04-20 02:08:34 +0530

    fibo

--- FibonacciSeries.py
+++ FibonacciSeries.py
@@ -1,27 +1,28 @@
 # 1.	Fibonacci Series up to N Terms
 # Write a program that prints the Fibonacci sequence up to the N-th term using a loop.
+#fibo logic
 
-# num=int(input("Enter a number of fibonacci series count"))
-# list=[0,1,1]
+num=int(input("Enter a number of fibonacci series count"))
+list=[0,1,1]
 
-# def fact(num):
-#     before=1
-#     current=1
-#     for i in range(0,num-(len(list))):
+def fibo(num):
+    before=1
+    current=1
+    for i in range(0,num-(len(list))):
         
-#         sum=before+current
-#         before=current
-#         current=sum
-#         list.append(sum)
+        sum=before+current
+        before=current
+        current=sum
+        list.append(sum)
 
-# fact(num)
-# print(list)
+fibo(num)
+print(list)
 
 #or
 num=int(input("Enter a number of fibonacci series count"))
 list=[0,1,1]
 
-def fact(num):
+def fibo2(num):
     before=1
     current=1
     while num-(len(list))>0:
@@ -31,7 +32,7 @@ def fact(num):
         current=sum
         list.append(sum)
 
-fact(num)
+fibo2(num)
 print(list)
 
 
